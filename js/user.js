2aeb9cb2539aaf95bd8baabf374444579b7fe0de
lisandrodalu <lisandrodalu@gmail.com>
2022-10-21 19:55:12 -0300

    primer intento de API cartes.io

--- js/user.js
+++ js/user.js
@@ -14,10 +14,16 @@ btnConfirm.addEventListener('click',function(e){
 sucursal.addEventListener('change',function(e){
     e.preventDefault();
       //Prueba de mostrar el mapa de cartes.io
-    /*const request = fetch('https://cartes.io/api/maps').then(res =>{
+    const request = fetch('https://cartes.io/api/maps/048eebe4-8dac-46e2-a947-50b6b8062fec?lat=-31.94411429512263&lng=-34.71679687500001&zoom=3',{
+        method: "GET",
+        headers: {'Accept': 'application/json'}
+      }
+    
+    ).then(res =>{
 
-    mapa.append(res)
-    });*/
+      console.log(res)
+    //mapa.append(res)
+    });
    
     labelTurno.style.display = "inline-block"
     listaTurno.style.display = "inline-block"
