4cf6f7f58d3b732abc02555354ded88cc277c793
lisandrodalu <lisandrodalu@gmail.com>
2022-10-22 20:42:00 -0300

    primer intento de carga de turnos y sucursales SIN API

--- ClienteWeb/js/user.js
+++ ClienteWeb/js/user.js
@@ -1,33 +1,85 @@
-
-
 var btnConfirm = document.getElementById("btn-confirm")
 var mapa = document.getElementById("map")
 var sucursal = document.getElementById("sucursales")
 var listaTurno = document.getElementById("lista_turnos")
 var labelTurno = document.getElementById("label_turnos")
+
+
 btnConfirm.addEventListener('click',function(e){
     e.preventDefault();
     alert("Turno Confirmado");
 
 
 })
+//'https://cartes.io/api/maps//f5c63822-a9f9-47cc-aeae-207c99a31a56
 sucursal.addEventListener('change',function(e){
     e.preventDefault();
       //Prueba de mostrar el mapa de cartes.io
-    const request = fetch('https://cartes.io/api/maps/048eebe4-8dac-46e2-a947-50b6b8062fec?lat=-31.94411429512263&lng=-34.71679687500001&zoom=3',{
-        method: "GET",
-        headers: {'Accept': 'application/json'}
+      var url = 'https://cartes.io/api/maps'
+    const request = fetch(url,{
+        method: "POST",
+        headers: {'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
       }
     
-    ).then(res =>{
+    ).then(res =>res.json())
+    .then(data => {
+      console.log(data)
+    }
+      
+    )
+    .catch(error => console.error(error));   
+  
+    //Cargar turnos de suscursal (comunicarse con api gateway)
+    console.log(sucursal.value)
+    //Elimino los turnos
+    let options = listaTurno.querySelectorAll('option')
+    options.forEach(o => o.remove());
 
-      console.log(res)
-    //mapa.append(res)
-    });
-   
+
+    cargarTurnos(sucursal.value);
+    
     labelTurno.style.display = "inline-block"
     listaTurno.style.display = "inline-block"
    
 });
+function cargarTurnos(idSucursal){
+let turnos1 = ["Turno 1","Turno 2"]
+let turnos2 = ["Turno 3","Turno 4"]
+cont =1;
+    if(idSucursal == 1){
+     
+     turnos1.forEach(element => {
+      let option  = document.createElement('option')
+      option.value = cont;
+      option.text = element;
+      listaTurno.appendChild(option);
+      cont+=1;
+     });
+    }
+    else{
+  
+      turnos2.forEach(element => {
+        let option  = document.createElement('option')
+        option.value = cont;
+        option.text = element;
+        listaTurno.appendChild(option);
+        cont+=1;
+       });
+    }
+
+}
+(function cargarSucursales(){
+     let option  = document.createElement('option')
+     option.value = 1;
+     option.text = "Sucursal 1";
+    sucursal.appendChild(option)
+    let option1  = document.createElement('option')
+     option1.value = 2;
+     option1.text = "Sucursal 2";
+    sucursal.appendChild(option1)
+    
+})();
 
 
