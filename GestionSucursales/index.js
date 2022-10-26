8afa02ae67e45d7f878e1438bb029eff8d599b93
lisandrodalu <lisandrodalu@gmail.com>
2022-10-25 21:20:21 -0300

    obtencion de una unica sucursal con id

--- GestionSucursales/index.js
+++ GestionSucursales/index.js
@@ -6,6 +6,30 @@ function getSucursales(res){
     res.write(JSON.stringify(sucursales)); // envio las sucursales
     res.end()
 }
+
+function getSucursal(res,idSucursal){
+    let sucursal = {}
+    sucursales.forEach(s => {
+        if(s.id == idSucursal){
+            sucursal = {
+                
+                    "id": s.id, 
+                    "lat": s.lat,
+                    "lng": s.lng,
+                     "name": s.name
+                    }
+                 
+            }
+        
+    });
+
+    res.writeHead(200,{'Content-Type':'application/json'}); // devuelvo json
+                    res.write(JSON.stringify(sucursal)); // envio las sucursales
+                    res.end()
+
+   
+
+}
 const server = http.createServer((req,res)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
@@ -16,13 +40,20 @@ const server = http.createServer((req,res)=>{
     
     //Logger
     console.log(`URL: ${url} - METHOD: ${method}`);
-    
+    let  parametros = url.split(":")
     switch(method){
+        
         case "GET":
-           
-            if(url == "/"){
+            console.log(parametros)
+            if(parametros.length == 1){ // todas las sucursales
                 getSucursales(res)
             }
+            if(parametros.length == 2){ // una unica suscursal
+               
+             let idSucursal = parametros[1];
+             getSucursal(res,idSucursal)
+
+             }
             break;    
     }
     })
