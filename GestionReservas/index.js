d54de2e0a9f44a805af46d76eabfd97972252e8c
lisandrodalu <lisandrodalu@gmail.com>
2022-10-28 16:15:45 -0300

    cambios en gestion de reservas en base a interfaces

--- GestionReservas/index.js
+++ GestionReservas/index.js
@@ -1,8 +1,7 @@
 const http = require('http');
-const fs = require('fs'); 
+const fs = require('fs');
+const path = require('url'); 
 var reservas =  require('./reservas.json');
-
-
 
 function bodyParser(request) {
     return new Promise((resolve, reject) => {
@@ -22,25 +21,28 @@ function bodyParser(request) {
     });
 }
   
-function getReservas(res){
-    res.writeHead(200,{'Content-Type':'application/json'}); // devuelvo json
-    res.write(JSON.stringify(reservas)); // envio las reservas
+function getTurnos(res,queryParams){
+    res.writeHead(200,{'Content-Type':'application/json'}); 
+    res.write(JSON.stringify(reservas)); 
     res.end()
 }
-function getReservasUsuario(res,idUsuario){
+function getReserva(res,idReserva){
     
-    reservasUsuario = []
-    reservas.forEach(element => {
-        if(element.userId == idUsuario){
-           reservasUsuario.push(element);
-        }        
-    });
-    
-    
-    res.writeHead(200,{'Content-Type':'application/json'}); // devuelvo json
-    res.write(JSON.stringify(reservasUsuario)); // envio las reservas
-    res.end()
+  let reserva = reservas.find(r => r.id == idReserva);
+  if(reserva!=undefined){
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify(reserva)); 
+  }else{
+    //Devuelvo error
+    res.writeHead(404,{'Content-Type':'application/json'});
+    respuesta = {
+
+    }
+    res.write(JSON.stringify(respuesta)); 
+  }
+  res.end();   
 }
+/*
 function getReservasUsuarioSucursal(res,idUsuario,idSucursal){
     reservasUsuarioSucursal = []
     reservas.forEach(element => {
@@ -53,14 +55,8 @@ function getReservasUsuarioSucursal(res,idUsuario,idSucursal){
     res.writeHead(200,{'Content-Type':'application/json'}); // devuelvo json
     res.write(JSON.stringify(reservasUsuarioSucursal)); // envio las reservas
     res.end()
-}
-function altaReserva(dateTime,email,branchid){
-    //agrego reserva al JSON
-    resultado = 1;//o 0
-    res.writeHead(200,{'Content-Type':'application/json'});
-    res.write(resultado);
-    res.end()
-}
+}*/
+
 
 function bajaReserva(idReserva){
     //eliminar reserva del JSON
@@ -70,7 +66,7 @@ function bajaReserva(idReserva){
     res.end()
 }
 
-function modificaReserva(req,res,idReserva){
+function altaReserva(req,res,idReserva){
 
     let resultado = 0;
     bodyParser(req)
@@ -103,13 +99,7 @@ function modificaReserva(req,res,idReserva){
             .catch(error => console.error(error));   
    
 }
-function getTurnosSucursal(){
 
-}
-
-function getTurnosUsuario(){
-
-}
 
 function verificaTurno(){
 
@@ -121,41 +111,40 @@ const server = http.createServer((req,res)=>{
     res.setHeader('Access-Control-Request-Method', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,DELETE,PATCH');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    
-    
+  
     const{url,method} = req;
-    
-    //Logger
+    const queryParams = path.parse(req.url, true).query;
+
     console.log(`URL: ${url} - METHOD: ${method}`);
-    let parametros = url.split("/:");
-    console.log(parametros)
-    if(parametros[0] == "/api/reserva"){
+    console.log(queryParams);
+ 
+    if(url.startsWith("/api/reserva")){
+        let parametros = url.split("/");
+        parametros = parametros.filter(el => el != '')   //filtro los vacios
+        console.log(parametros)
         switch(method){
             case "GET":
-                if(parametros.length == 1){ // todas las reservas
-                    getReservas(res)
+                if(parametros.length == 2 || (parametros.length>2 && parametros[2].startsWith("?"))){ 
+                    
+                    getTurnos(res,queryParams)   // turnos (chequear query params para filtar)
                 }
-                if(parametros.length ==2 ){ // todas las reservas de un usuario
-                    getReservasUsuario(res,parametros[1])
-                }
-                if(parametros.length ==3){ // todas las reservas de un usuario de una sucursal
-                    getReservasUsuarioSucursal(res,parametros[1],parametros[2])
+                if(parametros.length == 3 && !parametros[2].startsWith("?")){ // reserva vinculada a ese idReserva
+                    
+                    getReserva(res,parametros[2])
                 }
                 break;
-            case "PUT": //alta y modificacion
-          
-            modificaReserva(req,res,parametros[1]);
-             
+            case "POST": 
+                altaReserva(req,res,parametros[2]);
                 break;
-            case "DELETE"://baja de reserva
-                bajaReserva(res)
+            case "DELETE":
+                //bajaReserva(req,res,parametros[2])
                 break;    
         }
     }
     })
     
-    puerto = 8000;
-    server.listen(puerto);
-    console.log('Gestion Reservas',puerto);
+puerto = 8000;
+server.listen(puerto);
+console.log('Gestion Reservas',puerto);
 
    
