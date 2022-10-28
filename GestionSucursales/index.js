6c4af63fe6fb1d9395b8f3a9fb8f9cfb03dd2bf4
lisandrodalu <lisandrodalu@gmail.com>
2022-10-28 16:38:13 -0300

    cambio minimo en gestion de sucursales

--- GestionSucursales/index.js
+++ GestionSucursales/index.js
@@ -16,7 +16,7 @@ function getSucursal(res,idSucursal){
        res.write(JSON.stringify(sucursal)); // envio la sucursal  
      }
      else{
-        res.writeHead(400,{'Content-Type':'application/json'}); // devuelvo json
+        res.writeHead(404,{'Content-Type':'application/json'}); // devuelvo json
         res.write("Error, no se encuentra esa sucursal"); // envio la sucursal  
      }
    res.end()
@@ -27,32 +27,22 @@ const server = http.createServer((req,res)=>{
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Access-Control-Allow-Headers', '*');
     
-    const{ url,method} = req;
-    
+    const{ url,method} = req; 
     //Logger
     console.log(`URL: ${url} - METHOD: ${method}`);
-    let  parametros = url.split("/:")
-    console.log(parametros)
-    switch(method){
-        
-        case "GET":
-            if(parametros[0] == "/api/sucursales"){
-                if(parametros.length == 1){ // todas las sucursales
-                    getSucursales(res)
-                }
-                if(parametros.length == 2){ // una unica suscursal
-                   
-                 let idSucursal = parametros[1];
-                 getSucursal(res,idSucursal)
-    
-                 }
-
+    if(url.startsWith("/api/sucursales")){
+        let parametros = url.split("/");
+        parametros = parametros.filter(el => el != '')   //filtro los vacios
+        console.log(parametros)
+            if(parametros.length == 2){ // todas las sucursales
+                getSucursales(res)
             }
-           
-            break;    
+            if(parametros.length == 3){ // una unica suscursal
+                let idSucursal = parametros[2];
+                getSucursal(res,idSucursal)
+    
+            }    
     }
     })
-    
-    
     server.listen(8080);
     console.log('Gestion Sucursales',8080);
