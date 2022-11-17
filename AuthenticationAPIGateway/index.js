3c9af5829492c9481eff7169baa5244ee94bac40
augustomtt <65096407+augustomtt@users.noreply.github.com>
2022-11-17 11:49:11 -0300

    pruebatoken rama
    pruebatoken rama

--- AuthenticationAPIGateway/index.js
+++ AuthenticationAPIGateway/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
-const { auth } = require('express-oauth2-jwt-bearer');
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 
 const port = 8080
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
 const checkJwt = auth({
-  audience: '/api/auth',
-  issuerBaseURL: `https://dev-pn7zgl7ckp8stzea.us.auth0.com/`,
+  audience: 'https://dev-pn7zgl7ckp8stzea.us.auth0.com/api/v2/',
+  issuerBaseURL: `https://dev-pn7zgl7ckp8stzea.us.auth0.com`,
 });
+app.use(checkJwt) 
 
 // This route needs authentication
-app.get('/api/auth/private', checkJwt, function(req, res) {
+/*app.get('/api/auth/private', checkJwt, function(req, res) {
     res.json({
       message: 'Hello from a private endpoint! You need to be authenticated to see this.'
     });
+});*/
+app.get('/*', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
   });
+});
 
 app.listen(port);
 console.log("server listening on port " + port);
--- ClienteWeb/js/login.js
+++ ClienteWeb/js/login.js
@@ -1,50 +1,64 @@
- 
 /*<div class="">
           <button id="login" class="btn btn-primary center-block">Registrarse/Loggearte</button>
           <button id="logout" class="btn btn-primary">Invitado</button>
         </div>*/
-auth0.createAuth0Client({
-  responseType: "token id_token",
-  audience: "https://dev-pn7zgl7ckp8stzea.us.auth0.com/api/v2/", 
+auth0
+  .createAuth0Client({
+    //responseType: "code",
+    //audience: "https://dev-pn7zgl7ckp8stzea.us.auth0.com/api/v2/",
     domain: "dev-pn7zgl7ckp8stzea.us.auth0.com",
     clientId: "KMWqrOft0FpDJNmnzYriye6rOAQpXqQ9",
     authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  }).then(async (auth0Client) => {
+      redirect_uri: window.location.origin,
+    },
+  })
+  .then(async (auth0Client) => {
     // Assumes a button with id "login" in the DOM
     const loginButton = document.getElementById("login");
-  
+
     loginButton.addEventListener("click", (e) => {
       e.preventDefault();
-      console.log(window.location.origin)
+      console.log(window.location.origin);
       //console.log(authorizationParams.redirect_uri);
       //throw Error();
-      auth0Client.loginWithRedirect();
+      auth0Client.loginWithRedirect().then(token => {console.log(token)});
     });
-  
+
     if (location.search.includes("state=") && 
         (location.search.includes("code=") || 
         location.search.includes("error="))) {
-      await auth0Client.handleRedirectCallback();
-      window.history.replaceState({}, document.title, "/");
+      auth0Client.handleRedirectCallback().then((token) =>{
+        
+      auth0Client.getIdTokenClaims().then(sessionData =>
+        {
+            console.log(token)
+            console.log(sessionData)
+            if(sessionData) 
+            {
+              window.sessionStorage.setItem('token',sessionData.__raw)
+              window.sessionStorage.setItem('email',sessionData.email)
+              //window.sessionStorage.setItem('userId',generateUserId(sessionData.email))
+            }
+            window.history.replaceState({}, document.title, "/");
+        }); 
+      });
     }
-  
+
     // Assumes a button with id "logout" in the DOM
-    
+
     const logoutButton = document.getElementById("logout");
-  
+
     logoutButton.addEventListener("click", (e) => {
       e.preventDefault();
       auth0Client.logout();
     });
-  
+
     const isAuthenticated = await auth0Client.isAuthenticated();
     const userProfile = await auth0Client.getUser();
-  
+
     // Assumes an element with id "profile" in the DOM
     const profileElement = document.getElementById("profile");
-  /*
+    /*
     if (isAuthenticated) {
       profileElement.style.display = "block";
       profileElement.innerHTML = `
@@ -55,4 +69,3 @@ auth0.createAuth0Client({
       profileElement.style.display = "none";
     }*/
   });
-  
